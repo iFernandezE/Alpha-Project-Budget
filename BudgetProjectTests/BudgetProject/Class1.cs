5692a768fb46a3179c5a3277d2a049b1041f3430
Ivan Fernandez <ivan.fernandez@jala.university>
2023-02-07 17:12:16 -0400

    Project added

---
+++ BudgetProjectTests/BudgetProject/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace BudgetProject
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ BudgetProjectTests/BudgetProjectTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace BudgetProjectTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
